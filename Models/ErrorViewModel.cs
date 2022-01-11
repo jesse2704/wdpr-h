e50af0267d775863cfcb26f3d479e998cb1f3164
jesse2704 <jesse2704@users.noreply.github.com>
2022-01-11 16:20:00 +0100

    Clean project (Only dotnet new mvc has been executed)

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WDPR_Project_H.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
