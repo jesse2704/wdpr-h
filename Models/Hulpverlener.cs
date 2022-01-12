0637f4e908cb9b8447e6098014968f04296a1b57
jesse2704 <jesse2704@users.noreply.github.com>
2022-01-12 15:08:46 +0100

    Added HulpverlenerModel

---
+++ Models/Hulpverlener.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace wdpr_h.Models
+{
+    public class Hulpverlener
+    {
+        public Guid Id {get; set;}
+        public String Naam {get; set;}
+        public String Achternaam {get; set;}
+        public String Specialisme {get; set;}
+        public String Wachtwoord {get; set;}
+    }
+}
