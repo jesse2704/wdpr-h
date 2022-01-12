0f5a609468fb5a7bd2b1b02dce31ef2731ffa1ed
jesse2704 <jesse2704@users.noreply.github.com>
2022-01-12 15:16:34 +0100

    Made some changes in ModeratorModel && Created new Model named: Zelfhulpgroep

--- Models/Moderator.cs
+++ Models/Moderator.cs
@@ -4,8 +4,8 @@ namespace wdpr_h.Models
 {
     public class Moderator
     {
-        public Guid Id;
-        public String Naam;
-        public String Wachtwoord;
+        public Guid Id {get; set;}
+        public String Naam {get; set;}
+        public String Wachtwoord {get; set;}
     }
 }
---
+++ Models/Zelfhulpgroep.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace wdpr_h.Models
+{
+    public class Zelfhulpgroep
+    {
+        public Guid Id {get; set;}
+        public String Titel {get; set;}
+        public String Onderwerp {get; set;}
+        public String LeeftijdsCategorie {get; set;}
+    }
+}
