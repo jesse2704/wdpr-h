35c47504164dfa3ffabc177f0bbaba5d26760855
jesse2704 <jesse2704@users.noreply.github.com>
2022-01-12 21:05:00 +0100

    Scaffolded Identity Conformations && Set RegisterConformation DisplayConfirmAccountLink to False to Fight against immidiate login after register

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using wdpr_h.Areas.Identity.Data;
+
+[assembly: HostingStartup(typeof(wdpr_h.Areas.Identity.IdentityHostingStartup))]
+namespace wdpr_h.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("ApplicationDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            });
+        }
+    }
+}
