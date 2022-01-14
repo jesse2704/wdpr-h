1fbb6b42ba3246c24694057de2e99c70ae731c8e
jesse2704 <jesse2704@users.noreply.github.com>
2022-01-14 15:47:52 +0100

    Added DataAnnotations to ClientModel

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,15 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace wdpr_h.Models
 {
     public class Client
     {
+        [Key]
+        [Required]
         public Guid User { get; set; }
+        [Required]
+        [StringLength(15)]
         public String Nicknaam { get; set; }
+        [Required]
         public String LeeftijdsCategorie { get; set; }
+        [Required]
+        [StringLength(30)]
         public String Naam { get; set; }
+        [Required]
+        [StringLength(30)]
         public String Achternaam { get; set; }
+        [Required]
         public int HulpverlenerId { get; set; }
+        [Required]
         public String Wachtwoord { get; set; }
     }
 }
