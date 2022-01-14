17a90057228a2fd562b4cd83e72cdb7e5eb24d31
jesse2704 <jesse2704@users.noreply.github.com>
2022-01-14 15:49:19 +0100

    Added DataAnnotations to ClientZelfhulpgroepModel

--- Models/ClientZelfhulpgroep.cs
+++ Models/ClientZelfhulpgroep.cs
@@ -1,11 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace wdpr_h.Models
 {
     public class ClientZelfhulpgroep
     {
+        [Key]
+        public int Id {get;set;}
+
         //Tussentabel tussen Cliênt - Zelfhulpgroep
+        [Required]
         public Guid IdClient {get;set;} // Client ID 
+        [Required]
         public Guid IdGroep {get; set;} // Groep ID
     }
 }
