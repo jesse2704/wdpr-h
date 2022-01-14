8622ffd2c9c0bee30518a2fd2ab9fa866f858e35
jesse2704 <jesse2704@users.noreply.github.com>
2022-01-14 15:51:14 +0100

    Added DataAnnotations to HulpverlenerModel

--- Models/Hulpverlener.cs
+++ Models/Hulpverlener.cs
@@ -1,14 +1,24 @@
 using System;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace wdpr_h.Models
 {
     public class Hulpverlener
     {
+        [Key]
+        [Required]
         public Guid Id {get; set;}
+        [Required]
+        [StringLength(30)]
         public String Naam {get; set;}
+        [Required]
+        [StringLength(30)]
         public String Achternaam {get; set;}
+        [Required]
+        [StringLength(35)]
         public String Specialisme {get; set;}
+        [Required]
+        [StringLength(50)]
         public String Wachtwoord {get; set;}
     }
 }
