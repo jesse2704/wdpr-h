18b42c21cbf2bdd1bb11de4c2134f32b82738e50
jesse2704 <jesse2704@users.noreply.github.com>
2022-01-14 15:41:04 +0100

    Added DataAnnotations to ChatberichtModel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using wdpr_h.Models;
+using WDPR_Project_H.Models;
 
 namespace wdpr_h.Controllers
 {
