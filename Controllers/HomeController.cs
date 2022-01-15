b7fd750f11c9d5d99c9fb778dbfb42e38be244b5
jesse2704 <jesse2704@users.noreply.github.com>
2022-01-15 15:44:25 +0100

    removed unnecesary using

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using wdpr_h.Models;
-using WDPR_Project_H.Models;
 
 namespace wdpr_h.Controllers
 {
