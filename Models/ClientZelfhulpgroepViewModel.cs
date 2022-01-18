a6ede56b05f84f56acb3bd7f49b0511dec6cede9
jesse2704 <jesse2704@users.noreply.github.com>
2022-01-18 14:49:19 +0100

    Created new ViewModel

---
+++ Models/ClientZelfhulpgroepViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace wdpr_h.Models
+{
+    public class ClientZelfhulpgroepViewModel
+    {
+        public List<Zelfhulpgroep> Zelfhulpgroepen {get;set;}
+        public List<ClientZelfhulpgroep> ClientZelfhulpgroepen {get;set;}
+    }
+}
