2ad5073d203339167b92708bdb3fb8f8db0483d5
jesse2704 <jesse2704@users.noreply.github.com>
2022-01-22 13:16:54 +0100

    dsad

---
+++ Models/ModeratorViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace wdpr_h.Models
+{
+    public class ModeratorViewModel
+    {
+        public List<Hulpverlener> Hulpverleners {get;set;}
+        public List<Client> Clients {get;set;}
+    }
+}
